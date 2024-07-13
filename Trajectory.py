016370791d69e8f519c75c956c949f4dc19a829d
Ankit Khandelwal <ankit.khandelwal@viam.com>
2024-07-13 09:40:29 -0400

    this still works. return here if anything breaks. parabolic blends implemented in test file but now moving onto implementing it in path file

--- Trajectory.py
+++ Trajectory.py
@@ -191,7 +191,7 @@ class Trajectory:
         return None
     
     def ccw(self, A,B,C):
-                return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+        return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
 
     # Return true if line segments AB and CD intersect
     def intersect(self, A,B,C,D):
